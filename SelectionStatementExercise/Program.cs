7d4e701ffe9eed6cfde39acc3d40907309c29acf
tyler davis <tyler.davis432@gmail.com>
2023-09-05 20:15:06 -0400

    selection statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+             
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine()!);
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else {
+                Console.WriteLine("you got it !!");
+            } 
+           
+
+            
+               
+            
+                
+            
+          
+
+
+
         }
     }
 }
